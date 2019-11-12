2ea8bf887107c0a4ee8870b9353a1eb9711fedef
Alex <olexandrmai@gmail.com>
2019-11-12 23:33:08 +0200

    cr individual commponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { RightscreenComponent } from './rightscreen/rightscreen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidenavComponent,
+    RightscreenComponent
   ],
   imports: [
     BrowserModule
